0e380d0b1c5919ba2880b0544910961894b51889
ynnckth <yannickstreit@hotmail.com>
2019-10-21 17:25:16 +0200

    Updates

---
+++ precache-manifest.fd05163dde64383ef1155368d8708216.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "054f85ab9691740cd5459c9ac3aa1faa",
+    "url": "/synth/index.html"
+  },
+  {
+    "revision": "caf420eb962f957701c9",
+    "url": "/synth/static/css/main.b0bc87a4.chunk.css"
+  },
+  {
+    "revision": "754ff9879d6d864ce492",
+    "url": "/synth/static/js/2.157fec1f.chunk.js"
+  },
+  {
+    "revision": "caf420eb962f957701c9",
+    "url": "/synth/static/js/main.292c1899.chunk.js"
+  },
+  {
+    "revision": "941b4a578521a523e60c",
+    "url": "/synth/static/js/runtime-main.0637a986.js"
+  }
+]);
